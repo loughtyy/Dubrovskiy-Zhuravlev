3c3795a881f98d7934c0152e3225b30fcb375cff
Your Name <you@example.com>
2024-09-16 11:18:52 +0300

    ЛЗ 7 половина / оба

--- hello/app/views.py
+++ hello/app/views.py
@@ -4,12 +4,8 @@ from django.http import *
 
 def index(request):
  
- header = "Персональные данные" # обычная переменная
- langs = ["Английский", "Немецкий", "Испанский"] # массив
- user = {"name": "Максим,", "age": 30} # словарь
- addr = ("Виноградная", 23, 45) # кортеж
- data = {"header": header, "langs": langs, "user": user, "address": addr}
- return render(request, "index.html", context=data)
+ return render(request, "app/home.html")
+
 
 def about(request):
  return HttpResponse("About")
