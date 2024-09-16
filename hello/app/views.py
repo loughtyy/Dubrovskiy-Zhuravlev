af2dd38a6b124214ee6038da53e4de1037b9b9f7
Your Name <you@example.com>
2024-09-16 10:17:47 +0300

    16.09.24 Л6 Журавлёв.  Приобрёл общие сведения о создании и использовании шаблонов.

--- hello/app/views.py
+++ hello/app/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.http import *
 
 def index(request):
- return HttpResponse("Index")
+ 
+ header = "Персональные данные" # обычная переменная
+ langs = ["Английский", "Немецкий", "Испанский"] # массив
+ user = {"name": "Максим,", "age": 30} # словарь
+ addr = ("Виноградная", 23, 45) # кортеж
+ data = {"header": header, "langs": langs, "user": user, "address": addr}
+ return render(request, "index.html", context=data)
+
 def about(request):
  return HttpResponse("About")
 def contact(request):
@@ -17,3 +24,4 @@ def products(request, productid = 1):
 def users(request,id=1, name='Максим'):
     output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1} </h3> ".format(id,name)
     return HttpResponse(output)
+
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
  path('products/<int:productid>/', views.products),
  path('users/', views.users), 
  path('users/<int:id>/<str:name>/', views.users),
+ 
 
 ]
 
