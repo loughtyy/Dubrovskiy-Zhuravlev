06416986b1217083265307fd6b66433e5d68317f
Your Name <you@example.com>
2024-09-17 12:23:06 +0300

    17.09.24 Дубровский ПЗ 5 научился пользоваться специальными полями в формах

--- hello/app/forms.py
+++ hello/app/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class UserForm(forms.Form):
- file = forms.FileField(label="Файл")
+ city = forms.TypedMultipleChoiceField(label="Выберите город",
+ empty_value=None,
+ choices=((1, "Москва"),
+ (2, "Воронеж"),
+ (3, "Курск"),
+ (4, "Томск")))
