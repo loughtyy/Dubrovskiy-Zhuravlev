958081e4b056fc90568fca57b17e173c83b37a0d
Your Name <you@example.com>
2024-09-17 12:19:16 +0300

    17.09.24 Журавлёв Л8 Приобрел общее понятие о полях форм.

--- hello/app/forms.py
+++ hello/app/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
 class UserForm(forms.Form):
- name = forms.CharField(label="Имя клиента")
- age = forms.IntegerField(label="Возраст клиента")
+ file = forms.FileField(label="Файл")
--- hello/app/views.py
+++ hello/app/views.py
@@ -3,14 +3,9 @@ from django.shortcuts import render
 from django.http import *
 
 def index(request):
- if request.method == "POST":
-  name = request.POST.get("name") # получить значения поля Имя
-  age = request.POST.get("age") # значения поля Возраст
-  output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
-  return HttpResponse(output)
- else:
-   userform = UserForm()
-   return render(request, "app/index.html", {"form": userform})
+ userform = UserForm()
+ return render(request, "app/index.html", {"form": userform})
+
 
 
 def about(request):
