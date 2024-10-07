6901c595bbb7a2cd9d98e5f4f169e5c7c17c0fd0
Your Name <you@example.com>
2024-10-07 09:53:44 +0300

    07.10.24 ПЗ8 Журавлёв Научился создавать прототип сайта при помощи Django, запускать и останавливать сервер для разработки приложения, создавать модели данных для веб-приложения, использовать административную панель Django для управления сайтом.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from.models import Author, Book, Genre, Language, Status, BookInstance
+admin.site.register(Author) 
+admin.site.register(Book) 
+admin.site.register(Genre) 
+admin.site.register(Language) 
+admin.site.register(Status) 
+admin.site.register(BookInstance)
+
