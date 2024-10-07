637e436a99ef231c3a02672d2d56256819690153
Your Name <you@example.com>
2024-10-07 11:20:18 +0300

    07.10.24 ПЗ9 Дубровский 90%

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import re_path
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
+ path('', views.index, name='index'),
+ path('admin/', admin.site.urls),
+ re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+ re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+ re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,26 @@
+from .models import Book, Author, BookInstance, Genre
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 def index(request):
- return HttpResponse("Главная страница сайта Мир книг!")
+ num_books = Book.objects.all().count()
+ num_instances = BookInstance.objects.all().count()
+ num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+ num_authors = Author.objects.count()
+ return render(request, 'index.html', context={
+ 'num_books': num_books,
+ 'num_instances': num_instances,
+ 'num_instances_available': num_instances_available,
+ 'num_authors': num_authors})
+
+class BookListView(generic.ListView):
+ model = Book
+ paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
